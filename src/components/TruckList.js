17bad50b6b451782594702b4977cf0c6bd0c99b0
Daniel Ionita <danielionita88@yahoo.com>
2020-01-10 14:45:54 -0600

    render pictures on frontend

--- src/components/TruckList.js
+++ src/components/TruckList.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { Input,Menu} from 'semantic-ui-react'
+import {connect} from 'react-redux'
 
 
 class TruckList extends React.Component{
+
+    renderImages=(post)=>{
+        return post.photos_urls.map(url => <img src={`http://localhost:3000/${url}`}/>)
+    }
+
+    renderTrucks=()=>{
+       return this.props.posts.map(post=> <div>
+        <li key={post.id}>{post.title}</li>
+        {this.renderImages(post)}
+       </div>)
+    }
+
     render(){
         return<div>
             <Navbar/>
@@ -12,8 +25,15 @@ class TruckList extends React.Component{
                     <Input icon='search' placeholder='Search...' />
                 </Menu.Item>
             </Menu.Menu>
+            {this.renderTrucks()}
         </div>
     }
 }
 
-export default TruckList
+const mapStateToProps=state=>{
+    return{
+        posts: state.posts.allPosts
+    }
+}
+
+export default connect(mapStateToProps)(TruckList)
